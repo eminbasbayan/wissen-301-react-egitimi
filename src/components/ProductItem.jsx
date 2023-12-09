58e20d2781489222c8ff421fc27ae9ae81b0d56f
MustWis <147651626+MustWis@users.noreply.github.com>
2023-12-09 15:10:13 +0300

    dinamik props

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,16 +1,14 @@
 import "./ProductItem.css";
 
 function ProductItem(props) {
-  console.log(props);
+  const { imgUrl, productTitle, productPrice } = props.item;
 
   return (
     <div className="product-item">
-      <img src={props.imgUrl} alt="product image" />
+      <img src={imgUrl} alt="product image" />
       <div className="product-info">
-        <h3>{props.productTitle}</h3>
-        <strong>{props.productPrice}</strong> <br />
-        <span>{props.formattedDate}</span>
-        {props.name}
+        <h3>{productTitle}</h3>
+        <strong>{productPrice}</strong>
       </div>
     </div>
   );
