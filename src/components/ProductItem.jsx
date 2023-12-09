0075353fda3b7688cc214a8990fc221b7eda08c4
MustWis <147651626+MustWis@users.noreply.github.com>
2023-12-09 14:31:46 +0300

    react props

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,26 +1,16 @@
 import "./ProductItem.css";
 
-function ProductItem() {
-  const imgUrl =
-    "https://images.unsplash.com/photo-1570913149827-d2ac84ab3f9a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXBwbGV8ZW58MHx8MHx8fDA%3D";
-
-  const productTitle = "Elma";
-  const productPrice = 20;
-  const date = new Date(2023, 11, 9);
-
-  const day = date.getDay();
-  const month = date.getMonth();
-  const year = date.getFullYear();
-
-  const formattedDate = `${day}-${month}-${year}`;
+function ProductItem(props) {
+  console.log(props);
 
   return (
     <div className="product-item">
-      <img src={imgUrl} alt="product image" />
+      <img src={props.imgUrl} alt="product image" />
       <div className="product-info">
-        <h3>{productTitle}</h3>
-        <strong>{productPrice}</strong> <br />
-        <span>{formattedDate}</span>
+        <h3>{props.productTitle}</h3>
+        <strong>{props.productPrice}</strong> <br />
+        <span>{props.formattedDate}</span>
+        {props.name}
       </div>
     </div>
   );
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -2,11 +2,27 @@ import ProductItem from "./ProductItem";
 import "./Products.css";
 
 function Products() {
+  const imgUrl =
+    "https://images.unsplash.com/photo-1570913149827-d2ac84ab3f9a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXBwbGV8ZW58MHx8MHx8fDA%3D";
+
+  const productTitle = "Elma";
+  const productPrice = 20;
+  const date = new Date(2023, 11, 9);
+
+  const day = date.getDay();
+  const month = date.getMonth();
+  const year = date.getFullYear();
+
+  const formattedDate = `${day}-${month}-${year}`;
   return (
     <div className="products">
-      <ProductItem></ProductItem>
-      <ProductItem></ProductItem>
-      <ProductItem></ProductItem>
+      <ProductItem
+        imgUrl={imgUrl}
+        productTitle={productTitle}
+        productPrice={productPrice}
+        formattedDate={formattedDate}
+        name="Emin"
+      ></ProductItem>
     </div>
   );
 }
