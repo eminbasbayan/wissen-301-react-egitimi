93e0abfd683c3674f9e6f46d1940a10362fcb8f6
MustWis <147651626+MustWis@users.noreply.github.com>
2023-12-09 11:43:03 +0300

    ilk custom componentimizi oluşturduk

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import ProductItem from "./components/ProductItem";
+
+function App() {
+  return (
+    <div>
+      <h1>App Component</h1>
+      <ProductItem></ProductItem>
+      <ProductItem></ProductItem>
+      <ProductItem></ProductItem>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ProductItem.jsx
@@ -0,0 +1,11 @@
+function ProductItem() {
+  return (
+    <div className="product-item">
+      <img src="" alt="product image" />
+      <h3>Product Title</h3>
+      <strong>Product Price</strong>
+    </div>
+  );
+}
+
+export default ProductItem;
