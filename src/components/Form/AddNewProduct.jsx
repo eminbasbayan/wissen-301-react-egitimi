33cb0e6b370a940b60f340644e9fb9cd01b0e22b
MustWis <147651626+MustWis@users.noreply.github.com>
2023-12-12 19:38:51 +0300

    tek state tek fonksiyon ile input dinleme

--- src/components/Form/AddNewProduct.jsx
+++ src/components/Form/AddNewProduct.jsx
@@ -8,27 +8,12 @@ function AddNewProduct() {
     productImage: "",
   });
 
-  function handleNameChange(event) {
+  function handleProductChange(event) {
     const inputValue = event.target.value;
+    const name = event.target.name;
     setFormProductData({
       ...formProductData,
-      productName: inputValue,
-    });
-  }
-
-  function handlePriceChange(event) {
-    const inputValue = event.target.value;
-    setFormProductData({
-      ...formProductData,
-      productPrice: inputValue,
-    });
-  }
-
-  function handleImageChange(event) {
-    const inputValue = event.target.value;
-    setFormProductData({
-      ...formProductData,
-      productImage: inputValue,
+      [name]: inputValue,
     });
   }
 
@@ -38,15 +23,30 @@ function AddNewProduct() {
     <form className="product-form">
       <div className="form-item">
         <label htmlFor="name">Product Name</label>
-        <input type="text" id="name" onChange={handleNameChange} />
+        <input
+          type="text"
+          id="name"
+          onChange={handleProductChange}
+          name="productName"
+        />
       </div>
       <div className="form-item">
         <label htmlFor="price">Product Price</label>
-        <input type="number" id="price" onChange={handlePriceChange} />
+        <input
+          type="number"
+          id="price"
+          onChange={handleProductChange}
+          name="productPrice"
+        />
       </div>
       <div className="form-item">
         <label htmlFor="image">Product Image</label>
-        <input type="text" id="image" onChange={handleImageChange} />
+        <input
+          type="text"
+          id="image"
+          onChange={handleProductChange}
+          name="productImage"
+        />
       </div>
       <button>Add New Product</button>
     </form>
